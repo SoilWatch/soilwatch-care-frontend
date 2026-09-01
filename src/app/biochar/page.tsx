632e05139493485ca3c35823462ed6cb9fc0b48d
Collins Rop <crkimutai@kabarak.ac.ke>
2026-09-01 11:03:40 +0300

    fix: omit force dymnamic on github pages

--- src/app/biochar/page.tsx
+++ src/app/biochar/page.tsx
@@ -1,8 +1,6 @@
 import BiocharDashboard from "./BiocharDashboard";
 import { loadBiocharData } from "./ona";
 
-export const dynamic = "force-dynamic";
-
 export default async function BiocharPage() {
   const dataSource = await loadBiocharData();
   return <BiocharDashboard dataSource={dataSource} />;
--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -4,8 +4,6 @@ import { loadFieldTrialData } from "../biochar/fieldtrials";
 import KilnMapClient from "@/components/KilnMapClient";
 import { getT } from "@/lib/i18n/server";
 
-export const dynamic = "force-dynamic";
-
 const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? "";
 
 const C = {
